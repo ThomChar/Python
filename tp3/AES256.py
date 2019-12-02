a8d9aae0ee712f496bb20a86792c6db9871f89d1
= <remito@live.fr>
2019-12-02 11:47:45 +0100

    crypto AES256

---
+++ tp3/AES256.py
@@ -0,0 +1,29 @@
+from Crypto.Cipher import AES
+from password import *
+
+def crypto(password, data) :
+    key = password.encode()
+    cipher = AES.new(key, AES.MODE_EAX)
+
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(data.encode())
+
+    return ciphertext, tag, nonce
+
+def decrypt(password, ciphertext, tag, nonce) : 
+    key = password.encode()
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
+    try:
+        cipher.verify(tag)
+        print("The message is authentic:", plaintext.decode())
+    except ValueError:
+        print("Key incorrect or message corrupted")
+
+password = 'Sixteen byte key'
+data = "Hello, I m Rémi and I like chocolate."
+
+ciphertext, tag, nonce = crypto(password, data)
+decrypt(password, ciphertext, tag, nonce)
+
+# https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html
